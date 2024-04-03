95be1044b344b34b18b3d81ea4d8552af94e4e11
lizhenfeng <lizhenfeng@woda.ink>
2024-04-03 19:44:27 +0800

    for save

--- dsa/other/LRU.go
+++ dsa/other/LRU.go
@@ -1,113 +1,205 @@
 package main
 
-import "container/list"
+import (
+	"container/list"
+	"errors"
+)
 
-type LRU interface {
-	Get(key interface{}) (value interface{}, ok bool)
-	Set(key, value interface{})
-	Rem(key interface{})
-	Contains(key interface{}) bool
-	Len() int
-}
+// EvictCallback is used to get a callback when a cache entry is evicted
+type EvictCallback func(key interface{}, value interface{})
 
-type EvictCallBack func(key, value interface{})
-
-type SimpleLRU struct {
-	// 缓存容量
-	capacity int
-
-	// 散列表
-	items map[interface{}]*list.Element
-
-	// 驱逐队列 双向列表
+// LRU implements a non-thread safe fixed size LRU cache
+type LRU struct {
+	size      int
 	evictList *list.List
-
-	// 驱逐时的回调
-	onEvict EvictCallBack
+	items     map[interface{}]*list.Element
+	onEvict   EvictCallback
 }
 
-type KVP struct {
+// entry is used to hold a value in the evictList
+type entry struct {
 	key   interface{}
 	value interface{}
 }
 
-func NewSimpleLRU(capacity int, onEvict EvictCallBack) *SimpleLRU {
-	return &SimpleLRU{
-		capacity:  capacity,
-		items:     make(map[interface{}]*list.Element, 8),
+// NewLRU constructs an LRU of the given size
+func NewLRU(size int, onEvict EvictCallback) (*LRU, error) {
+	if size <= 0 {
+		return nil, errors.New("must provide a positive size")
+	}
+	c := &LRU{
+		size:      size,
 		evictList: list.New(),
+		items:     make(map[interface{}]*list.Element, 64),
 		onEvict:   onEvict,
 	}
+	return c, nil
 }
 
-func (s *SimpleLRU) Get(key interface{}) (value interface{}, ok bool) {
-	// 快速查找，散列表中是否存在
-	if e, ok := s.items[key]; ok {
-		// 移到队首
-		s.evictList.MoveToFront(e)
-		kvp, _ := e.Value.(*KVP)
-		return kvp.value, true
+// Purge is used to completely clear the cache.
+func (c *LRU) Purge() {
+	for k, v := range c.items {
+		if c.onEvict != nil {
+			c.onEvict(k, v.Value.(*entry).value)
+		}
+		delete(c.items, k)
 	}
-	return nil, false
+	c.evictList.Init()
 }
 
-func (s *SimpleLRU) Set(key, value interface{}) {
-	// 快速查找，散列表中是否存在
-	if e, ok := s.items[key]; ok {
-		kvp, _ := e.Value.(*KVP)
-		kvp.value = value
-		// 移到队首
-		s.evictList.MoveToFront(e)
+func (c *LRU) Add(key, value interface{}) {
+	// Check for existing item
+	if ent, ok := c.items[key]; ok {
+		c.evictList.MoveToFront(ent)
+		ent.Value.(*entry).value = value
 		return
 	}
 
-	// 插入新节点
-	kvp := &KVP{
-		key:   key,
-		value: value,
-	}
+	// Add new item
+	ent := &entry{key, value}
+	entry := c.evictList.PushFront(ent)
+	c.items[key] = entry
 
-	e := s.evictList.PushFront(kvp)
-	s.items[key] = e
-
-	// 新增缓存，判断容量是否已满
-	if s.evictList.Len() > s.capacity {
-		// 驱逐最老节点
-		s.remOldest()
+	evict := c.evictList.Len() > c.size
+	// Verify size not exceeded
+	if evict {
+		c.removeOldest()
 	}
+	return
 }
 
-func (s *SimpleLRU) Rem(key interface{}) {
-	if e, ok := s.items[key]; ok {
-		s.removeElement(e)
+// AddIfAbsent adds item only if key is not existed.
+func (c *LRU) AddIfAbsent(key interface{}, value interface{}) (priorValue interface{}) {
+	if ent, ok := c.items[key]; ok {
+		c.evictList.MoveToFront(ent)
+		if ent.Value == nil {
+			return nil
+		}
+		return ent.Value.(*entry).value
 	}
+	// Add new item
+	ent := &entry{key, value}
+	entry := c.evictList.PushFront(ent)
+	c.items[key] = entry
+
+	evict := c.evictList.Len() > c.size
+	// Verify size not exceeded
+	if evict {
+		c.removeOldest()
+	}
+	return nil
 }
 
-func (s *SimpleLRU) Contains(key interface{}) bool {
-	_, ok := s.items[key]
+// Get looks up a key's value from the cache.
+func (c *LRU) Get(key interface{}) (value interface{}, isFound bool) {
+	if ent, ok := c.items[key]; ok {
+		c.evictList.MoveToFront(ent)
+		if ent.Value.(*entry) == nil {
+			return nil, false
+		}
+		return ent.Value.(*entry).value, true
+	}
+	return
+}
+
+// Contains checks if a key is in the cache, without updating the recent-ness
+// or deleting it for being stale.
+func (c *LRU) Contains(key interface{}) (ok bool) {
+	_, ok = c.items[key]
 	return ok
 }
 
-func (s *SimpleLRU) Len() int {
-	return s.evictList.Len()
+// Peek returns the key value (or undefined if not found) without updating
+// the "recently used"-ness of the key.
+func (c *LRU) Peek(key interface{}) (value interface{}, isFound bool) {
+	var ent *list.Element
+	if ent, isFound = c.items[key]; isFound {
+		return ent.Value.(*entry).value, isFound
+	}
+	return nil, isFound
 }
 
-func (s *SimpleLRU) remOldest() {
-	// 取出最老节点
-	e := s.evictList.Back()
-	if e != nil {
-		s.removeElement(e)
+// Remove removes the provided key from the cache, returning if the
+// key was contained.
+func (c *LRU) Remove(key interface{}) (isFound bool) {
+	var ent *list.Element
+	if ent, isFound = c.items[key]; isFound {
+		c.removeElement(ent)
+		return
+	}
+	return
+}
+
+// RemoveOldest removes the oldest item from the cache.
+func (c *LRU) RemoveOldest() (key interface{}, value interface{}, ok bool) {
+	ent := c.evictList.Back()
+	if ent != nil {
+		c.removeElement(ent)
+		kv := ent.Value.(*entry)
+		return kv.key, kv.value, true
+	}
+	return nil, nil, false
+}
+
+// GetOldest returns the oldest entry
+func (c *LRU) GetOldest() (key interface{}, value interface{}, ok bool) {
+	ent := c.evictList.Back()
+	if ent != nil {
+		kv := ent.Value.(*entry)
+		return kv.key, kv.value, true
+	}
+	return nil, nil, false
+}
+
+// Keys returns a slice of the keys in the cache, from oldest to newest.
+func (c *LRU) Keys() []interface{} {
+	keys := make([]interface{}, len(c.items))
+	i := 0
+	for ent := c.evictList.Back(); ent != nil; ent = ent.Prev() {
+		keys[i] = ent.Value.(*entry).key
+		i++
+	}
+	return keys
+}
+
+// Len returns the number of items in the cache.
+func (c *LRU) Len() int {
+	return c.evictList.Len()
+}
+
+// Resize changes the cache size.
+func (c *LRU) Resize(size int) (evicted int) {
+	diff := c.Len() - size
+	if diff < 0 {
+		diff = 0
+	}
+	for i := 0; i < diff; i++ {
+		c.removeOldest()
+	}
+	c.size = size
+	return diff
+}
+
+// removeOldest removes the oldest item from the cache.
+func (c *LRU) removeOldest() {
+	ent := c.evictList.Back()
+	if ent != nil {
+		c.removeElement(ent)
 	}
 }
 
-func (s *SimpleLRU) removeElement(e *list.Element) {
-	s.evictList.Remove(e)
-
-	kvp, _ := e.Value.(*KVP)
-	delete(s.items, kvp.key)
-
-	// 回调
-	if s.onEvict != nil {
-		s.onEvict(kvp.key, kvp.value)
+// removeElement is used to remove a given list element from the cache
+func (c *LRU) removeElement(e *list.Element) {
+	c.evictList.Remove(e)
+	if e.Value == nil {
+		return
+	}
+	kv, ok := e.Value.(*entry)
+	if !ok {
+		return
+	}
+	delete(c.items, kv.key)
+	if c.onEvict != nil {
+		c.onEvict(kv.key, kv.value)
 	}
 }
